541ee9318240b0c6822331bbea26a1f990ae0588
Tulio <tulio.figueiredo@gmail.com>
2015-04-18 21:37:32 -0300

    Adicionado splashscreen; adicionado tela de cadastro de usuário, faltando trocar o ícone; adicionado ícones

---
+++ app/src/main/java/android/geeps/SplashScreen.java
@@ -0,0 +1,28 @@
+package android.geeps;
+
+/**
+ * Created by Túlio on 18/04/2015.
+ */
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends Activity {
+
+    private static final int SPLASHTIMEOUT = 3000;
+
+    @Override
+    protected final void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, ClienteEntregadorActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SplashScreen.SPLASHTIMEOUT);
+    }
+}
