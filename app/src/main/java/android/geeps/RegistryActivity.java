3e540159ccb2f2f25ca2b7a564f8a202e97fa398
Tulio <tulio.figueiredo@gmail.com>
2015-04-19 18:52:32 -0300

    Refatorando tela de sign up e splash screen

--- app/src/main/java/android/geeps/RegistryActivity.java
+++ app/src/main/java/android/geeps/RegistryActivity.java
@@ -2,9 +2,17 @@ package android.geeps;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class RegistryActivity extends Activity {
@@ -17,7 +25,7 @@ public class RegistryActivity extends Activity {
         setContentView(R.layout.sign_up);
 
         souCliente = (Button) findViewById(R.id.btn_registry);
-
+        fillSpinnerCoutries();
         clienteListener();
     }
 
@@ -31,4 +39,45 @@ public class RegistryActivity extends Activity {
         });
     }
 
+
+
+    private void fillSpinnerCoutries() {
+
+        Resources r = getApplicationContext().getResources();
+        TypedArray countrieCodes = r.obtainTypedArray(R.array.countries);
+
+        List<String> country = new ArrayList<String>();
+        List<String> code = new ArrayList<String>();
+
+        for (int i = 0; i < countrieCodes.length(); i++) {
+            int id = countrieCodes.getResourceId(i, 0);
+            code.add(r.getStringArray(id)[0]);
+            country.add(r.getStringArray(id)[1]);
+        }
+
+        countrieCodes.recycle();
+
+        final List<String> fCode = code;
+        final List<String> fCountry = country;
+
+        final Spinner p = (Spinner) findViewById(R.id.countries);
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, country);
+        p.setAdapter(dataAdapter);
+        p.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                String selectedCountry = (String) p.getSelectedItem();
+                int selectedPosition = fCountry.indexOf(selectedCountry);
+                String correspondingCode = fCode.get(selectedPosition);
+                // Here is your corresponding country code
+                System.out.println(correspondingCode);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+                // TODO Auto-generated method stub
+            }
+        });
+    }
 }
