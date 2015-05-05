74fe7489f6f8bb41aa27eee59f420b361cadd0ac
Igor Candiea <igorcandeia@gmail.com>
2015-05-04 23:00:24 -0300

    Comentários

--- app/src/main/java/android/geeps/core/UserOrder.java
+++ app/src/main/java/android/geeps/core/UserOrder.java
@@ -6,6 +6,7 @@ import android.graphics.drawable.Drawable;
  * Created by Igor on 04/05/2015.
  */
 public class UserOrder {
+    // Por enquanto ta icone, nome (title) e uma descricao
     public final Drawable icon;       // the drawable for the ListView item ImageView
     public final String title;        // the text for the ListView item title
     public final String description;  // the text for the ListView item description
