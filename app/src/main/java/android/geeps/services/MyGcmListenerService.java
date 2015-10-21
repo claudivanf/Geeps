5803e7549fe3701f9b83ba6f2b952f76c53188ab
Tulio <tulio.figueiredo@gmail.com>
2015-10-21 12:09:12 -0300

    tentando corrigir bug no recebimento de notificacao

--- app/src/main/java/android/geeps/services/MyGcmListenerService.java
+++ app/src/main/java/android/geeps/services/MyGcmListenerService.java
@@ -1,15 +1,11 @@
 package android.geeps.services;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.geeps.util.GeepsNotification;
-import android.geeps.util.SPManager;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.gcm.GcmListenerService;
-
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * Service do GCM, faz com que receba mensagens do gcm em background.
@@ -19,11 +15,13 @@ public class MyGcmListenerService extends GcmListenerService {
     @Override
     public void onMessageReceived(String from, Bundle data) {
 
-        String message = data.getString("PEDIDO_NOTIFICATION");
+        String message = data.getString("ENTREGADOR_NOTIFICATION");
+        System.out.println("##################################### ");
 
-        if(message == null){
-            message = data.getString("ENTREGADOR_NOTIFICATION");
+        if(message == null){ //CLIENTE
+            message = data.getString("PEDIDO_NOTIFICATION");
             String entregadorId = data.getString("ENTREGADOR_ID");
+            Log.d("MSG", entregadorId + "");
 
             // comeca a pegar a localização em tempo real
             Intent intent = new Intent(this, EntregadorService.class);
@@ -34,8 +32,9 @@ public class MyGcmListenerService extends GcmListenerService {
 
             GeepsNotification.sendNotification(this, message, 2);
 
-        } else {
-            GeepsNotification.sendNotification(this, message, 1);
+        } else { //ENTREGADOR
+            String entregadorId = data.getString("ENTREGADOR_ID");
+            GeepsNotification.sendNotification(this, message, 2);
             // TODO arrumar um jeito de recarregar a tela de listar pedidos
         }
     }
